e59c781bb92dff9168200f09e375b05454df14c4
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-21 18:16:46 +0330

    create model documentation and fix documentation

--- documentation/admin.py
+++ documentation/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import City, Genre, Author, Documentation
+
+
+class DocumentationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'type', 'status')
+    list_filter = ('type', 'status')
+    search_fields = ('name', 'author')
+    list_per_page = 20
+    ordering = ('name',)
+    actions = ['make_available', 'make_unavailable']
+
+
+admin.site.register(City)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Documentation, DocumentationAdmin)
--- documentation/models.py
+++ documentation/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Documentation(models.Model):
+    TYPE_CHOICES = (
+        ('B', _('Book')),
+        ('A', _('Article')),
+    )
+    STATUS_CHOICES = (
+        ('A', _('Available')),
+        ('U', _('Unavailable')),
+    )
+
+    name = models.CharField(max_length=250)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default='B')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='A')
+    price = models.FloatField()
+    description = models.TextField()
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE, related_name='author')
+    city = models.ForeignKey(to=City, on_delete=models.CASCADE, related_name='city')
+    genre = models.ForeignKey(to=Genre, on_delete=models.CASCADE, related_name='genre')
+
+    def __str__(self):
+        return str(self.name)
