bc8c09b88922040d881f101ebd345459e8fdc1d7
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-21 14:19:01 +0330

    Add create_otp and is_otp_match functions

---
+++ core/services.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import redis_services
+
+
+def create_otp(key, header=None, length=5, **options):
+    otp = str(int.from_bytes(os.urandom(int(length / 2)), sys.byteorder))
+    otp = otp[:length]
+    if len(otp) < length:
+        otp = otp.zfill(length)
+
+    redis_services.set(redis_services.make_key(key, header), otp, **options)
+    return otp
+
+
+def is_otp_match(otp, key, header=None, **options):
+    result_otp = redis_services.get(redis_services.make_key(key, header))
+    if result_otp is None or result_otp.decode('utf-8') != otp:
+        return False
+
+    return True
