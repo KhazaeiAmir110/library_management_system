c488aed561bc6467b7fc6d3f36c1bc20de43c737
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-21 14:53:58 +0330

    fix rest

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_redis',
     'rest_framework_simplejwt',
+    'django_filters',
 
     'user',
 ]
@@ -132,3 +133,11 @@ AUTH_USER_MODEL = 'user.User'
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = '6379'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
