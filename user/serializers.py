6bfc0be29e3dabb25235543306871b1e0ca41665
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-21 14:32:42 +0330

    Add custom serializers and authentication-related serializers
    - Added CustomTokenObtainPairSerializer to customize token authentication with additional fields and validation logic.
    - Added UserGenerateOTPSerializer for phone number input during OTP generation.
    - Added UserVerifyOTPSerializer for phone number and OTP code validation.
    - Added VeryfiSerializer with a code field for integer validation.
    
    These serializers enhance the functionality and customization options for authentication and OTP generation and verification.
    
    Resolves: #ISSUE_NUMBER

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,10 @@
 from .models import User
-from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, PasswordField
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework import exceptions, serializers
+
+from rest_framework_simplejwt.settings import api_settings
 
 
 class UserPublicSerializer(serializers.ModelSerializer):
@@ -18,3 +23,52 @@ class UserBackOfficeSerializer(serializers.ModelSerializer):
             'id', 'username', 'phone_number', 'name', 'age', 'image', 'email', 'is_active',
             'is_staff', 'status',
         )
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields[self.username_field] = serializers.CharField()
+        self.fields["password"] = PasswordField(allow_null=True, required=False)
+
+    def validate(self, attrs):
+        authenticate_kwargs = {
+            self.username_field: attrs[self.username_field],
+        }
+        try:
+            authenticate_kwargs["request"] = self.context["request"]
+        except KeyError:
+            pass
+
+        self.user = authenticate(**authenticate_kwargs)
+
+        if not api_settings.USER_AUTHENTICATION_RULE(self.user):
+            raise exceptions.AuthenticationFailed(
+                self.error_messages["no_active_account"],
+                "no_active_account",
+            )
+
+        refresh = self.get_token(self.user)
+
+        data = dict()
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
+
+
+class UserGenerateOTPSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=11)
+
+
+class UserVerifyOTPSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=11)
+    otp_code = serializers.CharField(max_length=5)
+
+
+class VeryfiSerializer(serializers.ModelSerializer):
+    code = serializers.IntegerField(max_value=5)
