99d820dabf59c4bff945a4fd78fc1704004cd913
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-21 14:41:20 +0330

    fix urls

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import UserBackOfficeViewSet
+from .views import UserBackOfficeViewSet, UserViewSet
 from rest_framework import routers
 
 backoffice_router = routers.SimpleRouter()
 backoffice_router.register(r'backoffice', UserBackOfficeViewSet)
 
+router = routers.DefaultRouter()
+router.register(r'', UserViewSet)
+
 urlpatterns = [
     path('', include(backoffice_router.urls)),
+    path('', include(router.urls)),
 ]
