839d1bc2283475e55aa4762b559f1f32327ea0d2
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-22 09:46:08 +0330

    fix urls

--- documentation/urls.py
+++ documentation/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import DocumentationViewSet, DocumentationBackOfficeViewSet,DocumentationListView
+from .views import DocumentationViewSet, DocumentationBackOfficeViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'', DocumentationViewSet)
-router.register(r'list', DocumentationListView, basename='list')
 
 backoffice_router = routers.SimpleRouter()
 backoffice_router.register(r'', DocumentationBackOfficeViewSet)
