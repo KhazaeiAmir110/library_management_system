614033b966b836b1216026b0ef43cd5fd4967e60
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-22 10:29:19 +0330

    create model reservation

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ReservationTest
 
-# Register your models here.
+
+class ReservationTestAdmin(admin.ModelAdmin):
+    list_display = ['user', 'documentation', 'created', 'updated', 'pyment', ]
+    list_filter = ['created', 'updated']
+    search_fields = ['user__username', 'documentation__title']
+    date_hierarchy = 'created'
+
+
+admin.site.register(ReservationTest, ReservationTestAdmin)
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
+from documentation.models import Documentation
 
-# Create your models here.
+
+class ReservationTest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    documentation = models.ForeignKey(Documentation, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    pyment = models.DecimalField(max_digits=10, decimal_places=1, name='pyment')
+
+    def __str__(self):
+        return str(self.pyment)
